6e19510f828bbc8dbe2749f246cd72102c761c48
Henrique Rodrigues <henrique.rodrigues@etu.iut-tlse3.fr>
2022-05-09 15:46:16 +0200

    Update supprimer-moyen.php

--- www/codes snippet/supprimer-moyen.php
+++ www/codes snippet/supprimer-moyen.php
@@ -6,28 +6,28 @@
 
 
 	//LIAISON A LA BDD
-	require("codes snippet/GestionBdd.php");
-	$bdd = new GestionBdd();
+// 	require("codes snippet/GestionBdd.php");
+// 	$bdd = new GestionBdd();
 
-	//SUPPRESSION D'UN MOYEN
-	//si un id a bien ete transmis
-	if (isset($_POST["id_moyen"])) {
-		$id = $_POST["id_moyen"];
-		if (!empty($id)) {
-			// si l'id d'un moyen est defini, on le supprime
-			$req= $bdd->supprimerMoyen($id);
+// 	//SUPPRESSION D'UN MOYEN
+// 	//si un id a bien ete transmis
+// 	if (isset($_POST["id_moyen"])) {
+// 		$id = $_POST["id_moyen"];
+// 		if (!empty($id)) {
+// 			// si l'id d'un moyen est defini, on le supprime
+// 			$req= $bdd->supprimerMoyen($id);
 
-			//si un fichier est defini
-			if (isset($_POST["urlfic"])) {
-				$fichier = $_POST["urlfic"];
-				if (!empty($fichier)) {
-						//SUPPRESSION DU FICHIER ASSOCIE
-						$fichier = explode('/wp-content', $fichier);
-						$fichier = str_replace('/', DIRECTORY_SEPARATOR, "/wp-content".$fichier[1]);
-						if (file_exists(getcwd().$fichier))
-							unlink(getcwd().$fichier);
-				}
-			}
-		}
-	}
-?>
+// 			//si un fichier est defini
+// 			if (isset($_POST["urlfic"])) {
+// 				$fichier = $_POST["urlfic"];
+// 				if (!empty($fichier)) {
+// 						//SUPPRESSION DU FICHIER ASSOCIE
+// 						$fichier = explode('/wp-content', $fichier);
+// 						$fichier = str_replace('/', DIRECTORY_SEPARATOR, "/wp-content".$fichier[1]);
+// 						if (file_exists(getcwd().$fichier))
+// 							unlink(getcwd().$fichier);
+// 				}
+// 			}
+// 		}
+// 	}
+// ?>
