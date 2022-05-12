70b1937c3159fb6629c930394b484df6581631e9
Henrique Rodrigues <henrique.rodrigues@etu.iut-tlse3.fr>
2022-05-12 13:22:49 +0200

    pdate

--- www/App/Intranet/Offre/supprimer-offre.php
+++ www/App/Intranet/Offre/supprimer-offre.php
@@ -4,29 +4,28 @@
     * Améliorations à apporter :
     */
 
+//LIAISON A LA BDD
+require("App/GestionBdd.php");
+$bdd = new GestionBdd();
 
-	//LIAISON A LA BDD
-	require("App/GestionBdd.php");
-	$bdd = new GestionBdd();
+//SUPPRESSION D'UNE OFFRE
+if (isset($_POST["id_offre"])) {
+	$id = $_POST["id_offre"];
+	if (!empty($id)) {
+		// si l'id d'une offre est defini, on la supprime
+		$req= $bdd->supprimerOffre($id);
 
-	//SUPPRESSION D'UNE OFFRE
-	if (isset($_POST["id_offre"])) {
-		$id = $_POST["id_offre"];
-		if (!empty($id)) {
-			// si l'id d'une offre est defini, on la supprime
-			$req= $bdd->supprimerOffre($id);
-
-			//si un fichier existe, on le supprime
-			if (isset($_POST["urlfic"])) {
-				$fichier = $_POST["urlfic"];
-				if  (!empty($fichier)) {
-						//SUPPRESSION DU FICHIER ASSOCIE
-						$fichier = explode('/wp-content', $fichier);
-						$fichier = str_replace('/', DIRECTORY_SEPARATOR, "/wp-content".$fichier[1]);
-						if (file_exists(getcwd().$fichier))
-							unlink(getcwd().$fichier);
-				}
+		//si un fichier existe, on le supprime
+		if (isset($_POST["urlfic"])) {
+			$fichier = $_POST["urlfic"];
+			if  (!empty($fichier)) {
+					//SUPPRESSION DU FICHIER ASSOCIE
+					$fichier = explode('/wp-content', $fichier);
+					$fichier = str_replace('/', DIRECTORY_SEPARATOR, "/wp-content".$fichier[1]);
+					if (file_exists(getcwd().$fichier))
+						unlink(getcwd().$fichier);
 			}
 		}
 	}
+}
 ?>
