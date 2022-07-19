a13c122143392a90d081e361864265c1477ce5d5
[Bird] <tmangeard@gmx.fr>
2022-07-19 16:33:46 +0200

    wp_mail attachement ok

--- www/App/Intranet/ZRR/depot-zrr.php
+++ www/App/Intranet/ZRR/depot-zrr.php
@@ -13,7 +13,8 @@ if (!is_user_logged_in()) {
   exit();
 }
 
-$saveok = 0;
+$echec_file = false;
+$valider = false;
 if (isset($_POST['valider'])) {
   //on importe GestionBdd.php
   require("App/GestionBdd.php");
@@ -29,24 +30,22 @@ if (isset($_POST['valider'])) {
   $mail = $current_user->user_email;
   $user_id = get_current_user_id();
   $date_fin = $_POST['date_fin'];
-  $url = dirname(__DIR__) . "/ZRR/uploads/" . strtolower($_POST['nom']) . strtolower($_POST['prenom']) . $user_id . ".zip";
-  $file = 0;
+  $url = WP_CONTENT_DIR . "/uploads/zrr/" . strtolower($_POST['nom']) . strtolower($_POST['prenom']) . $user_id . ".zip";
+  $new_ask = false;
   if (filesize($_FILES['fichier']['tmp_name']) < 10000000) {
     if (!file_exists($url)) {
       $req = $bdd->ajouterDemande(strtolower($_POST['nom']), strtolower($_POST['prenom']), $mailArrivant, $mail, $url, $date_fin, $tuteur, $date_arrivee, $statut_arrivant, $etablissement_accueil);
-      $file = 1;
+      $new_ask = true;
     }
     move_uploaded_file($_FILES['fichier']['tmp_name'], $url);
-    if ($file == 0) {
+    if ($new_ask == false) {
       wp_mail('acces_zrr_ica@insa-toulouse.fr', 'Mise à jour de fichier ZRR', $name . ' a mis le fichier ZRR de ' . $_POST['prenom'] . ' ' . $_POST['nom'] . ' à jour', 'Bonjour,', array($url));
       $req = $bdd->resetDossier($url);
-    }
-    if ($file == 1) {
+    } else {
       wp_mail('acces_zrr_ica@insa-toulouse.fr', 'Nouvelle demande ZRR', $name . ' a fait une demande ZRR pour ' . $_POST['prenom'] . ' ' . $_POST['nom'] . ' : ' . site_url() . '/demandes-zrr/. La fin de mission est estimée à ' . $_POST['date_fin'], 'Bonjour,', array($url));
     }
-    $saveok = 2;
   } else {
-    $saveok = 1;
+    $echec_file = true;
 ?>
     <div id="echec">
       <p style="color:white"> &nbsp; Échec de la soumission de votre dossier. Le document déposé dépasse les 7 Mo.</p><br>
@@ -60,28 +59,30 @@ if (isset($_POST['valider'])) {
 <p>Pour plus de détails sur les documents nécessaires pour la demande ZRR, reportez vous à la page <a href='<?= site_url(); ?>/documents/zrr-site-de-toulouse/'><?= site_url(); ?>/documents/zrr-site-de-toulouse/</a></p>
 
 <?php
-if ($saveok == 1) {
+if ($valider == true) {
+  if ($echec_file == true) {
 ?>
-  <div id="echec">
-    <p style="color:white"> &nbsp; Échec de la soumission de votre dossier</p><br>
-  </div>
+    <div id="echec">
+      <p style="color:white"> &nbsp; Échec de la soumission de votre dossier</p><br>
+    </div>
+  <?php
+  } else {
+  ?>
+    <div id="confirmation">
+      <p style="color:white"> &nbsp; Dossier soummis avec succès!</p><br>
+    </div>
 <?php
-} elseif ($saveok == 2) {
-?>
-  <div id="confirmation">
-    <p style="color:white"> &nbsp; Dossier soummis avec succès!</p><br>
-  </div>
-<?php
+  }
 }
 ?>
 
-<form id="inscription4" name="zrr" method="post" action="'<?= site_url(); ?>/documents/zrr-site-de-toulouse/depot-dossier-zrr/" enctype="multipart/form-data">
-  Prénom de l'arrivant (nécéssaire) : <input type="text" name="prenom" required />
-  Nom de l'arrivant (nécéssaire) : <input type="text" name="nom" required /><br /><br />
+<form id="inscription4" name="zrr" method="post" action="<?= site_url(); ?>/documents/zrr-site-de-toulouse/depot-dossier-zrr/" enctype="multipart/form-data">
+  Prénom de l'arrivant<abbr class="required" title="required">*</abbr> : <input type="text" name="prenom" required />
+  Nom de l'arrivant<abbr class="required" title="required">*</abbr> : <input type="text" name="nom" required /><br /><br />
   <label for="statut">Statut de l'arrivant : </label><select id="statut" name="statut_arrivant" required />
   <option value="Administratif"> Administratif</option>
   <option value="Assistant ingénieur"> Assistant ingénieur</option>
-  <option value="Attaché temporaire d\'enseignement et de recherche"> Attaché temporaire d\'enseignement et de recherche</option>
+  <option value="Attaché temporaire d\'enseignement et de recherche"> Attaché temporaire d'enseignement et de recherche</option>
   <option value="Chargé de recherche"> Chargé de recherche</option>
   <option value="Chercheur invité"> Chercheur invité</option>
   <option value="Directeur de recherche"> Directeur de recherche</option>
@@ -104,17 +105,17 @@ if ($saveok == 1) {
   <option value="Stagiaire"> Stagiaire</option>
   <option value="Technicien"> Technicien</option>
   </select><br /><br />
-  <label for="etablissement">Etablissement d\'accueil : </label><select id="statut" name="etablissement_accueil" required />
+  <label for="etablissement">Etablissement d'accueil : </label><select id="statut" name="etablissement_accueil" required />
   <option value="INSA Toulouse"> INSA Toulouse</option>
   <option value="ISAE Supaero"> ISAE Supaero</option>
   <option value="Université Toulouse Paul Sabatier"> Université Toulouse Paul Sabatier</option>
   <option value="IMT Mines Albi"> IMT Mines ALbi</option>
   <option value="CNRS"> CNRS</option>
   </select><br /><br />
-  Nom et prénom du tuteur (nécéssaire) : <input type="text" name="nom_prenom_tuteur" required /><br />
-  Adresse E-mail de l'arrivant (nécéssaire) : <input type="email" name="mail" required /><br /><br />
-  Date d'arrivée (nécessaire) :<input type="date" name="date_arrivee" required /><br /><br />
-  Date estimée de fin de mission (nécessaire) :<input type="date" name="date_fin" required /><br />
+  Nom et prénom du tuteur<abbr class="required" title="required">*</abbr> : <input type="text" name="nom_prenom_tuteur" required /><br />
+  Adresse E-mail de l'arrivant<abbr class="required" title="required">*</abbr> : <input type="email" name="mail" required /><br /><br />
+  Date d'arrivée<abbr class="required" title="required">*</abbr> :<input type="date" name="date_arrivee" required /><br /><br />
+  Date estimée de fin de mission<abbr class="required" title="required">*</abbr> :<input type="date" name="date_fin" required /><br />
   <br />
   Dépôt fichier de l'archive zip (moins de <em>7Mo</em>! ) contenant:<br><br>
   -Fichier excel<br>
