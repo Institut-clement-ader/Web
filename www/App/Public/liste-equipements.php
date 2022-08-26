8d00606fb9c13c59bf7123c12608ce3f795db4b4
[Bird] <tmangeard@gmx.fr>
2022-08-26 09:14:23 +0200

    update list equipement

--- www/App/Public/liste-equipements.php
+++ www/App/Public/liste-equipements.php
@@ -1,96 +1,75 @@
 <?php
 
-	/**
-    * Améliorations à apporter :
-    */
+/**
+ * Améliorations à apporter :
+ */
 
-	//détection de langue courante de la page
-  $currentlang = get_bloginfo('language');
+//détection de langue courante de la page
+$currentlang = get_bloginfo('language');
 
-  if(strpos($currentlang,'fr')!==false){
-    include('App/lang-fr.php');
-  }elseif(strpos($currentlang,'en')!==false){
-    include('App/lang-en.php');
-  }else{
-    echo("échec de reconnaissance de la langue");
-  }
-
-
-  require("App/GestionBdd.php");
-  $bdd = new GestionBdd();
-
-	//NOMBRE D'EQUIPEMENTS
-	$nb_moyens = $bdd->nombreEquipement();
-	//Affichage selon le nombre de resultats
-	if ($nb_moyens == 0)
-		$nb_moyens= TXT_AUCUN_EQUIPEMENT;
-	if ($nb_moyens > 1)
-		echo $nb_moyens.TXT_EQUIPEMENTS_EQUIPEMENT."<br><br>";
-	else
-		echo $nb_moyens.TXT_EQUIPEMENT_EQUIPEMENT."<br><br>";
-
-	//ANALYSE PHYSICO-CHIMIQUE
-	$categorie ="Analyse physico-chimique";
-	$res = $bdd->analyseListeEquipement($categorie);
-	//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
-	if ($res[0][0] > 0)
-  		?>
-		<br><p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?=TXT_PHYSICO_EQUIPEMENT?></strong></p>
-		<?php
-		echo do_shortcode('[pods name="moyen" where="categorie=\'Analyse physico-chimique\'" template="Tableau des moyens" limit="1000"]');
+if (strpos($currentlang, 'fr') !== false) {
+	include('App/lang-fr.php');
+} elseif (strpos($currentlang, 'en') !== false) {
+	include('App/lang-en.php');
+} else {
+	echo ("échec de reconnaissance de la langue");
+}
 
 
-	//CARACTERISATION MECANIQUE
-	$categorie ="Caractérisation mécanique";
-	$res = $bdd->analyseListeEquipement($categorie);
-	//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
-	if ($res[0][0] > 0)
-		?>
-		<br><p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?=TXT_CMECANIQUE_EQUIPEMENT?></strong></p>
-		<?php
-		echo do_shortcode('[pods name="moyen" where="categorie=\'Caractérisation mécanique\'" template="Tableau des moyens" limit="1000"]');
+require("App/GestionBdd.php");
+$bdd = new GestionBdd();
 
+//NOMBRE D'EQUIPEMENTS
+$nb_moyens = $bdd->nombreEquipement();
+//Affichage selon le nombre de resultats
+if ($nb_moyens == 0)
+	$nb_moyens = TXT_AUCUN_EQUIPEMENT;
+if ($nb_moyens > 1)
+	echo $nb_moyens . TXT_EQUIPEMENTS_EQUIPEMENT . "<br><br>";
+else
+	echo $nb_moyens . TXT_EQUIPEMENT_EQUIPEMENT . "<br><br>";
 
-	//CONTROLE ET MESURE DES PIECES FABRIQUEES
-	$categorie ="Contrôle et mesure des pièces fabriquées";
-	$res = $bdd->analyseListeEquipement($categorie);
-	//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
-	if ($res[0][0] > 0)
-		?>
-		<br><p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?=TXT_CONTROLE_EQUIPEMENT?></strong></p>
-		<?php
-		echo do_shortcode('[pods name="moyen" where="categorie=\'Contrôle et mesure des pièces fabriquées\'" template="Tableau des moyens" limit="1000"]');
-
-
-	//FABRICATION
-	$categorie ="Fabrication";
-	$res = $bdd->analyseListeEquipement($categorie);
-	//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
-	if ($res[0][0] > 0)
-		?>
-		<br><p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?=TXT_FABRICATION_EQUIPEMENT?></strong></p>
-		<?php
-		echo do_shortcode('[pods name="moyen" where="categorie=\'Fabrication\'" template="Tableau des moyens" limit="1000"]');
-
-
-	//SIMULATION NUMERIQUE
-	$categorie ="Simulation numérique";
-	$res = $bdd->analyseListeEquipement($categorie);
-	//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
-	if ($res[0][0] > 0)
-		?>
-		<br><p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?=TXT_SIMUNUMERIQUE_EQUIPEMENT?></strong></p>
-		<?php
-		echo do_shortcode('[pods name="moyen" where="categorie=\'Simulation numérique\'" template="Tableau des moyens" limit="1000"]');
-
-
-	//TRAITEMENTS THERMIQUES
-	$categorie ="Traitements thermiques";
-	$res = $bdd->analyseListeEquipement($categorie);
-	//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
-	if ($res[0][0] > 0)
-		?>
-		<br><p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?=TXT_THERMIQUES_EQUIPEMENT?></strong></p>
-		<?php
-		echo do_shortcode('[pods name="moyen" where="categorie=\'Traitements thermiques\'" template="Tableau des moyens" limit="1000"]');
+//CARACTERISATION Matériaux
+$categorie = "Caractérisation matériaux";
+$res = $bdd->analyseListeEquipement($categorie);
+//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
+if ($res[0][0] > 0)
 ?>
+<br>
+<p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?= TXT_CMECANIQUE_EQUIPEMENT ?></strong></p>
+<?php
+echo do_shortcode('[pods name="moyen" where="categorie=\'Caractérisation matériaux\'" template="Tableau des moyens" limit="1000"]');
+
+//ESSAIS
+$categorie = "Essais";
+$res = $bdd->analyseListeEquipement($categorie);
+//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
+if ($res[0][0] > 0)
+?>
+<br>
+<p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?= TXT_ESSAIS ?></strong></p>
+<?php
+echo do_shortcode('[pods name="moyen" where="categorie=\'Essais\'" template="Tableau des moyens" limit="1000"]');
+
+//FABRICATION
+$categorie = "Fabrication";
+$res = $bdd->analyseListeEquipement($categorie);
+//Affichage du titre puis de la liste d'offres (en utilisant un template Pods)
+if ($res[0][0] > 0)
+?>
+<br>
+<p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?= TXT_FABRICATION_EQUIPEMENT ?></strong></p>
+<?php
+echo do_shortcode('[pods name="moyen" where="categorie=\'Fabrication\'" template="Tableau des moyens" limit="1000"]');
+
+
+//INSTRUMENTATION
+$categorie = "Instrumentation";
+$res = $bdd->analyseListeEquipement($categorie);
+//Affichage du titre puis de la liste des équipiments (en utilisant un template Pods)
+if ($res[0][0] > 0)
+?>
+<br>
+<p style='font-size: 1.33em; padding-left: 45px; color: #ba2133;'><strong><?= TXT_INSTRUMENTATION ?></strong></p>
+<?php
+echo do_shortcode('[pods name="moyen" where="categorie=\'Instrumentation\'" template="Tableau des moyens" limit="1000"]');
--- www/App/lang-fr.php
+++ www/App/lang-fr.php
@@ -1,192 +1,191 @@
 <?php
- 
-    //Début annuaire.php (français)
- 
-    define('TXT_NOM_ANNUAIRE', 'Nom');
-    define('TXT_STATUT_ANNUAIRE', 'Statut');
-    define('TXT_GROUPE_ANNUAIRE', 'Groupe');
-    define('TXT_ETABLISSEMENT_ANNUAIRE', "Établissement d'origine");
-    define('TXT_CHERCHER_ANNUAIRE', 'Chercher un membre...');
- 
-    //Fin annuaire.php (français)
 
-    //Début publications-acl.php (français)
+//Début annuaire.php (français)
 
-    define('TXT_VOIRTOUT_PUBLIACL', 'Voir tous les ACL de l\'Institut Clément Ader');
-    define('TXT_RAVANCEE_PUBLIACL', 'Recherche avancée');
-    define('TXT_ANNEE_PUBLIACL', 'Année');
-    define('TXT_AUTEURDOC_PUBLIACL', 'Auteurs et titre du document ');
-    define('TXT_LIENS_PUBLIACL', 'Liens');
+define('TXT_NOM_ANNUAIRE', 'Nom');
+define('TXT_STATUT_ANNUAIRE', 'Statut');
+define('TXT_GROUPE_ANNUAIRE', 'Groupe');
+define('TXT_ETABLISSEMENT_ANNUAIRE', "Établissement d'origine");
+define('TXT_CHERCHER_ANNUAIRE', 'Chercher un membre...');
 
-    //Fin publications-acl.php (français)
+//Fin annuaire.php (français)
 
-    //Début search-publi.php (français)
+//Début publications-acl.php (français)
 
-    define('TXT_GROUPE_RAVANCEE', 'Groupe : ');
-    define('TXT_TOUSGROUPE_RAVANCEE', ' Tous les groupes ');
-    define('TXT_TYPEDOC_RAVANCEE', 'Type de document');
-    define('TXT_PUBLICATIONS_RAVANCEE', 'Publications');
-    define('TXT_ART_RAVANCEE', 'Article dans une revue');
-    define('TXT_COMM_RAVANCEE', 'Communication dans un congr&egrave;s');
-    define('TXT_INV_RAVANCEE', 'Communication invit&eacute;');
-    define('TXT_CHAPITRE_RAVANCEE', 'Chapitre d\'ouvrage');
-    define('TXT_AUTRE_RAVANCEE', 'Autre publication');
-    define('TXT_OUVRAGE_RAVANCEE', 'Ouvrage (y compris &eacute;dition critique et traduction)');
-    define('TXT_DIRECTION_RAVANCEE', 'Direction d\'ouvrage, proceedings, dossier');
-    define('TXT_POSTER_RAVANCEE', 'Poster');
-    define('TXT_BREVET_RAVANCEE', 'Brevet');
-    define('TXT_NONPUBLI_RAVANCEE', 'Documents non publi&eacute;s');
-    define('TXT_PREPUBLI_RAVANCEE', 'Pr&eacute;-publication, document de travail');
-    define('TXT_RAPPORT_RAVANCEE', 'Rapport');
-    define('TXT_TRAVAUXUNIV_RAVANCEE', 'Travaux universitaires');
-    define('TXT_THESE_RAVANCEE', 'Th&egrave;se');
-    define('TXT_HDR_RAVANCEE', 'HDR');
-    define('TXT_COURS_RAVANCEE', 'Cours');
-    define('TXT_DONNEES_RAVANCEE', 'Donn&eacute;es de recherche');
-    define('TXT_IMAGE_RAVANCEE', 'Image');
-    define('TXT_VIDEO_RAVANCEE', 'Vid&eacute;o');
-    define('TXT_CARTE_RAVANCEE', 'Carte');
-    define('TXT_SON_RAVANCEE', 'Son');
-    define('TXT_PERIODE_RAVANCEE', 'Période : ');
-    define('TXT_DE_RAVANCEE', 'De ');
-    define('TXT_A_RAVANCEE', ' à ');
-    define('TXT_RECHERCHER_RAVANCEE', 'Rechercher');
+define('TXT_VOIRTOUT_PUBLIACL', 'Voir tous les ACL de l\'Institut Clément Ader');
+define('TXT_RAVANCEE_PUBLIACL', 'Recherche avancée');
+define('TXT_ANNEE_PUBLIACL', 'Année');
+define('TXT_AUTEURDOC_PUBLIACL', 'Auteurs et titre du document ');
+define('TXT_LIENS_PUBLIACL', 'Liens');
 
-    //Fin search-publi.php (français)
+//Fin publications-acl.php (français)
 
-    //Début publications.php (français)
+//Début search-publi.php (français)
 
-    define('TXT_ANNEE_PUBLICATIONS', 'Année');
-    define('TXT_TYPE_PUBLICATIONS', 'Type');
-    define('TXT_AUTEURDOC_PUBLICATIONS', 'Auteurs et titre du document ');
-    define('TXT_LIENS_PUBLICATIONS', 'Liens');
-    define('TXT_PRODUCTIONS_PUBLICATIONS', 'Productions scientifiques du laboratoire : ');
-    define('TXT_AUCUNEPROD_PUBLICATIONS', 'Aucune contribution trouvée correspondant à votre recherche.');
-    define('TXT_CONTRIBUTION_PUBLICATIONS', ' contribution trouvée correspondant à votre recherche.');
-    define('TXT_TELECHARGER_PUBLICATIONS', 'Télécharger au format Excel');
-    define('TXT_CONTRIBUTIONS_PUBLICATIONS', ' contributions trouvées correspondant à votre recherche.');
+define('TXT_GROUPE_RAVANCEE', 'Groupe : ');
+define('TXT_TOUSGROUPE_RAVANCEE', ' Tous les groupes ');
+define('TXT_TYPEDOC_RAVANCEE', 'Type de document');
+define('TXT_PUBLICATIONS_RAVANCEE', 'Publications');
+define('TXT_ART_RAVANCEE', 'Article dans une revue');
+define('TXT_COMM_RAVANCEE', 'Communication dans un congr&egrave;s');
+define('TXT_INV_RAVANCEE', 'Communication invit&eacute;');
+define('TXT_CHAPITRE_RAVANCEE', 'Chapitre d\'ouvrage');
+define('TXT_AUTRE_RAVANCEE', 'Autre publication');
+define('TXT_OUVRAGE_RAVANCEE', 'Ouvrage (y compris &eacute;dition critique et traduction)');
+define('TXT_DIRECTION_RAVANCEE', 'Direction d\'ouvrage, proceedings, dossier');
+define('TXT_POSTER_RAVANCEE', 'Poster');
+define('TXT_BREVET_RAVANCEE', 'Brevet');
+define('TXT_NONPUBLI_RAVANCEE', 'Documents non publi&eacute;s');
+define('TXT_PREPUBLI_RAVANCEE', 'Pr&eacute;-publication, document de travail');
+define('TXT_RAPPORT_RAVANCEE', 'Rapport');
+define('TXT_TRAVAUXUNIV_RAVANCEE', 'Travaux universitaires');
+define('TXT_THESE_RAVANCEE', 'Th&egrave;se');
+define('TXT_HDR_RAVANCEE', 'HDR');
+define('TXT_COURS_RAVANCEE', 'Cours');
+define('TXT_DONNEES_RAVANCEE', 'Donn&eacute;es de recherche');
+define('TXT_IMAGE_RAVANCEE', 'Image');
+define('TXT_VIDEO_RAVANCEE', 'Vid&eacute;o');
+define('TXT_CARTE_RAVANCEE', 'Carte');
+define('TXT_SON_RAVANCEE', 'Son');
+define('TXT_PERIODE_RAVANCEE', 'Période : ');
+define('TXT_DE_RAVANCEE', 'De ');
+define('TXT_A_RAVANCEE', ' à ');
+define('TXT_RECHERCHER_RAVANCEE', 'Rechercher');
 
-   //Fin publications.php (français)
+//Fin search-publi.php (français)
 
-   //Début liste-equipements.php (français)
+//Début publications.php (français)
 
-   define('TXT_EQUIPEMENTS_EQUIPEMENT', ' équipements disponibles sur Toulouse et Albi.');
-   define('TXT_EQUIPEMENT_EQUIPEMENT', ' équipement disponible sur Toulouse et Albi.');
-   define('TXT_AUCUN_EQUIPEMENT', 'Aucun');
-   define('TXT_PHYSICO_EQUIPEMENT', 'Analyse physico-chimique');
-   define('TXT_CMECANIQUE_EQUIPEMENT', 'Caractérisation mécanique');
-   define('TXT_CONTROLE_EQUIPEMENT', 'Contrôle et mesure des pièces fabriquées');
-   define('TXT_FABRICATION_EQUIPEMENT', 'Fabrication');
-   define('TXT_SIMUNUMERIQUE_EQUIPEMENT', 'Simulation numérique');
-   define('TXT_THERMIQUES_EQUIPEMENT', 'Traitements thermiques');
+define('TXT_ANNEE_PUBLICATIONS', 'Année');
+define('TXT_TYPE_PUBLICATIONS', 'Type');
+define('TXT_AUTEURDOC_PUBLICATIONS', 'Auteurs et titre du document ');
+define('TXT_LIENS_PUBLICATIONS', 'Liens');
+define('TXT_PRODUCTIONS_PUBLICATIONS', 'Productions scientifiques du laboratoire : ');
+define('TXT_AUCUNEPROD_PUBLICATIONS', 'Aucune contribution trouvée correspondant à votre recherche.');
+define('TXT_CONTRIBUTION_PUBLICATIONS', ' contribution trouvée correspondant à votre recherche.');
+define('TXT_TELECHARGER_PUBLICATIONS', 'Télécharger au format Excel');
+define('TXT_CONTRIBUTIONS_PUBLICATIONS', ' contributions trouvées correspondant à votre recherche.');
 
-   //Fin liste-equipements.php (français)
+//Fin publications.php (français)
 
-   //Début liste-offres-emploi.php (français)
+//Début liste-equipements.php (français)
 
-   define('TXT_OFFRES_EMPLOI', ' offres d\'emploi trouvées.');
-   define('TXT_AUCUNE_EMPLOI', 'Aucune');
-   define('TXT_OFFRE_EMPLOI', ' offre d\'emploi trouvée.');
-   define('TXT_CDD_EMPLOI', 'Contrats à Durée Déterminée');
-   define('TXT_DOCTORAT_EMPLOI', 'Doctorat');
-   define('TXT_PDOCTORAT_EMPLOI', 'Post-doctorat');
-   define('TXT_PERMANENTS_EMPLOI', 'Postes permanents');
-   define('TXT_STAGES_EMPLOI', 'Stages');
+define('TXT_EQUIPEMENTS_EQUIPEMENT', ' équipements disponibles sur Toulouse et Albi.');
+define('TXT_EQUIPEMENT_EQUIPEMENT', ' équipement disponible sur Toulouse et Albi.');
+define('TXT_AUCUN_EQUIPEMENT', 'Aucun');
+define('TXT_PHYSICO_EQUIPEMENT', 'Analyse physico-chimique');
+define('TXT_CMECANIQUE_EQUIPEMENT', 'Caractérisation matériaux');
+define('TXT_CONTROLE_EQUIPEMENT', 'Contrôle et mesure des pièces fabriquées');
+define('TXT_FABRICATION_EQUIPEMENT', 'Fabrication');
+define('TXT_SIMUNUMERIQUE_EQUIPEMENT', 'Simulation numérique');
+define('TXT_THERMIQUES_EQUIPEMENT', 'Traitements thermiques');
+define('TXT_ESSAIS', 'Essais');
+define('TXT_INSTRUMENTATION', 'Instrumentation');
 
-   //Fin liste-offres-emploi.php (français)
+//Fin liste-equipements.php (français)
 
-   //Début excel-theses.php (français)
+//Début liste-offres-emploi.php (français)
 
-   define('TXT_TELECHARGER_EXCEL', 'Télécharger au format Excel');
+define('TXT_OFFRES_EMPLOI', ' offres d\'emploi trouvées.');
+define('TXT_AUCUNE_EMPLOI', 'Aucune');
+define('TXT_OFFRE_EMPLOI', ' offre d\'emploi trouvée.');
+define('TXT_CDD_EMPLOI', 'Contrats à Durée Déterminée');
+define('TXT_DOCTORAT_EMPLOI', 'Doctorat');
+define('TXT_PDOCTORAT_EMPLOI', 'Post-doctorat');
+define('TXT_PERMANENTS_EMPLOI', 'Postes permanents');
+define('TXT_STAGES_EMPLOI', 'Stages');
 
-   //Fin excel-theses.php (français)
+//Fin liste-offres-emploi.php (français)
 
-   //Début thesesencours.php (français)
+//Début excel-theses.php (français)
 
-   define('TXT_AUCUNE_TENCOURS', 'Aucune');
-   define('TXT_THESES_TENCOURS', ' thèses en cours.');
-   define('TXT_THESE_TENCOURS', ' thèse en cours.');
+define('TXT_TELECHARGER_EXCEL', 'Télécharger au format Excel');
 
-   //Fin thesesencours.php (français)
+//Fin excel-theses.php (français)
 
-   //Début publications-tous-acl.php (français)
+//Début thesesencours.php (français)
 
-   define('TXT_ANNEE_PUBLITACL', 'Année');
-   define('TXT_AUTEURDOC_PUBLITACL', 'Auteurs et titre du document ');
-   define('TXT_LIENS_PUBLITACL', 'Liens');
-   define('TXT_TELECHARGER_PUBLITACL', 'Télécharger au format Excel');
-   define('TXT_RAVANCEE_PUBLITACL', 'Recherche avancée');
+define('TXT_AUCUNE_TENCOURS', 'Aucune');
+define('TXT_THESES_TENCOURS', ' thèses en cours.');
+define('TXT_THESE_TENCOURS', ' thèse en cours.');
 
-   //fIN publications-tous-acl.php (français)
-   
-   //Début Partie réservation (français)
+//Fin thesesencours.php (français)
 
-   define('TXT_LUNDI', 'Lundi');
-   define('TXT_MARDI', 'Mardi');
-   define('TXT_MERCREDI', 'Mercredi');
-   define('TXT_JEUDI', 'Jeudi');
-   define('TXT_VENDREDI', 'Vendredi');
-   define('TXT_SAMEDI', 'Samedi');
-   define('TXT_DIMANCHE', 'Dimanche');
-   define('TXT_JANVIER', 'Janvier');
-   define('TXT_FEVRIER', 'Février');
-   define('TXT_MARS', 'Mars');
-   define('TXT_AVRIL', 'Avril');
-   define('TXT_MAI', 'Mai');
-   define('TXT_JUIN', 'Juin');
-   define('TXT_JUILLET', 'Juillet');
-   define('TXT_AOUT', 'Aout');
-   define('TXT_SEPTEMBRE', 'Septembre');
-   define('TXT_OCTOBRE', 'Octobre');
-   define('TXT_NOVEMBRE', 'Novembre');
-   define('TXT_DECEMBRE', 'Décembre');
-   define('TXT_TITRE_RESER', 'Titre de la réservation');
-   define('TXT_NOM_UTI', 'Nom de l\'utilisateur');
-   define('TXT_NOM_MOYEN', 'Nom du moyen');
-   define('TXT_DATE_DEB', 'Date de début de la réservation');
-   define('TXT_HEURE_DEB', 'Heure de début de la réservation');
-   define('TXT_DATE_FIN', 'Date de fin de la réservation');
-   define('TXT_HEURE_FIN', 'Heure de fin de la réservation');
-   define('TXT_CONSULTER', 'consulter');
-   define('LIEN_CALENDRIER', '/calendrier-reservations');
-   define('LIEN_RESERVATION', '/voir-une-reservation');
-   define('TXT_PAS_RESER', 'Il n\'y a pas de réservation');
-   define('TXT_CAT_MOYEN', 'Catégorie des moyens :');
-   define('TXT_MOYEN', 'Moyens :');
-   define('TXT_ERREUR_MOYEN', 'Il y a déja une réservation à cette date avec le moyen');
-   define('TXT_ERREUR_UTI', 'Il y a déja une réservation à cette date avec l\'utilisateur.');
-   define('TXT_TITRE', 'Titre :');
-   define('TXT_NOM_UTI_ADD', 'Nom de l\'utilisateur :');
-   define('TXT_NOM_MOYEN_ADD', 'Nom du moyen :');
-   define('TXT_DATE_DEB_ADD', 'Date de début :');
-   define('TXT_HEURE_DEB_ADD', 'Heure de début :');
-   define('TXT_DATE_FIN_ADD', 'Date de fin :');
-   define('TXT_HEURE_FIN_ADD', 'Heure de fin :');
-   define('TXT_ERREUR_DATE', 'Jour de début est plus vieux que Jour de fin.');
-   define('TXT_ERREUR_HEURE', 'Heure de début est plus vieux que Heure de fin dans la meme journée.');
-   define('TXT_RAISON', 'Raison :');
-   define('TXT_ENCADRANT', 'Encadrant (doctorants) :');
-   define('TXT_ENCADRANT_SEUL', 'Encadrant :');
-   define('TXT_AXE', 'Groupe :');
-   define('TXT_DESC', 'Description :');
-   define('TXT_AJOUTER', 'Ajouter');
-   define('TXT_VALIDER', 'Valider');
-   define('TXT_ANNULER', 'Annuler');
-   define('TXT_MODIFIER', 'Modifier');
-   define('TXT_SUPPRIMER', 'Supprimer');
-   define('TXT_REUSSI', 'Réservation enregistrée, vous pouvez retourner sur le calendrier.');
-   define('TXT_MAINTENANCE', 'maintenance');
-   define('TXT_ESSAI', 'essai');
-   define('TXT_FORMATION', 'formation');
-   define('TXT_REUSSI_MODIF', 'Modification enregistrée, vous pouvez retourner sur le calendrier.');
-   define('TXT_CAT_MOYEN', 'Catégorie des moyens :');
-   define('TXT_MOYEN', 'Moyens :');
-   define('LIEN_JOURNEE', '/reservation-de-la-journee');
-   define('LIEN_CALENDRIER', '/calendrier-reservations');
-   define('LIEN_MODIFIER', '/modifier-une-reservation');
-   define('LIEN_SUPPRIMER', '/supprimer-une-reservation');
-   define('TXT_NON_RESA_GROUPE', 'Pas de réservation');
+//Début publications-tous-acl.php (français)
+
+define('TXT_ANNEE_PUBLITACL', 'Année');
+define('TXT_AUTEURDOC_PUBLITACL', 'Auteurs et titre du document ');
+define('TXT_LIENS_PUBLITACL', 'Liens');
+define('TXT_TELECHARGER_PUBLITACL', 'Télécharger au format Excel');
+define('TXT_RAVANCEE_PUBLITACL', 'Recherche avancée');
+
+//fIN publications-tous-acl.php (français)
+
+//Début Partie réservation (français)
+
+define('TXT_LUNDI', 'Lundi');
+define('TXT_MARDI', 'Mardi');
+define('TXT_MERCREDI', 'Mercredi');
+define('TXT_JEUDI', 'Jeudi');
+define('TXT_VENDREDI', 'Vendredi');
+define('TXT_SAMEDI', 'Samedi');
+define('TXT_DIMANCHE', 'Dimanche');
+define('TXT_JANVIER', 'Janvier');
+define('TXT_FEVRIER', 'Février');
+define('TXT_MARS', 'Mars');
+define('TXT_AVRIL', 'Avril');
+define('TXT_MAI', 'Mai');
+define('TXT_JUIN', 'Juin');
+define('TXT_JUILLET', 'Juillet');
+define('TXT_AOUT', 'Aout');
+define('TXT_SEPTEMBRE', 'Septembre');
+define('TXT_OCTOBRE', 'Octobre');
+define('TXT_NOVEMBRE', 'Novembre');
+define('TXT_DECEMBRE', 'Décembre');
+define('TXT_TITRE_RESER', 'Titre de la réservation');
+define('TXT_NOM_UTI', 'Nom de l\'utilisateur');
+define('TXT_NOM_MOYEN', 'Nom du moyen');
+define('TXT_DATE_DEB', 'Date de début de la réservation');
+define('TXT_HEURE_DEB', 'Heure de début de la réservation');
+define('TXT_DATE_FIN', 'Date de fin de la réservation');
+define('TXT_HEURE_FIN', 'Heure de fin de la réservation');
+define('TXT_CONSULTER', 'consulter');
+define('LIEN_CALENDRIER', '/reservations_moyens');
+define('LIEN_RESERVATION', '/voir-une-reservation');
+define('TXT_PAS_RESER', 'Il n\'y a pas de réservation');
+define('TXT_CAT_MOYEN', 'Catégorie des moyens :');
+define('TXT_MOYEN', 'Moyens :');
+define('TXT_ERREUR_MOYEN', 'Il y a déja une réservation à cette date avec le moyen');
+define('TXT_ERREUR_UTI', 'Il y a déja une réservation à cette date avec l\'utilisateur.');
+define('TXT_TITRE', 'Titre :');
+define('TXT_NOM_UTI_ADD', 'Nom de l\'utilisateur :');
+define('TXT_NOM_MOYEN_ADD', 'Nom du moyen :');
+define('TXT_DATE_DEB_ADD', 'Date de début :');
+define('TXT_HEURE_DEB_ADD', 'Heure de début :');
+define('TXT_DATE_FIN_ADD', 'Date de fin :');
+define('TXT_HEURE_FIN_ADD', 'Heure de fin :');
+define('TXT_ERREUR_DATE', 'Jour de début est plus vieux que Jour de fin.');
+define('TXT_ERREUR_HEURE', 'Heure de début est plus vieux que Heure de fin dans la meme journée.');
+define('TXT_RAISON', 'Raison :');
+define('TXT_ENCADRANT', 'Encadrant (doctorants) :');
+define('TXT_ENCADRANT_SEUL', 'Encadrant :');
+define('TXT_AXE', 'Groupe :');
+define('TXT_DESC', 'Description :');
+define('TXT_AJOUTER', 'Ajouter');
+define('TXT_VALIDER', 'Valider');
+define('TXT_ANNULER', 'Annuler');
+define('TXT_MODIFIER', 'Modifier');
+define('TXT_SUPPRIMER', 'Supprimer');
+define('TXT_REUSSI', 'Réservation enregistrée, vous pouvez retourner sur le calendrier.');
+define('TXT_MAINTENANCE', 'maintenance');
+define('TXT_ESSAI', 'essai');
+define('TXT_FORMATION', 'formation');
+define('TXT_REUSSI_MODIF', 'Modification enregistrée, vous pouvez retourner sur le calendrier.');
+define('TXT_CAT_MOYEN', 'Catégorie des moyens :');
+define('TXT_MOYEN', 'Moyens :');
+define('LIEN_JOURNEE', '/reservation-de-la-journee');
+define('LIEN_MODIFIER', '/modifier-une-reservation');
+define('LIEN_SUPPRIMER', '/supprimer-une-reservation');
+define('TXT_NON_RESA_GROUPE', 'Pas de réservation');
       
    //fIN Partie réservation (français)
-
-?>
